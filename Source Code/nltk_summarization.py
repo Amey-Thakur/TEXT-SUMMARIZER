71cb794aef227b6c67e78248b58e164577de5d33
Amey Thakur <ameythakur20@gmail.com>
2026-01-05 19:36:43 -0500

    Text Summarizer

--- Source Code/nltk_summarization.py
+++ Source Code/nltk_summarization.py
@@ -16,8 +16,17 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 import heapq  
 
 def nltk_summarizer(raw_text):
+	"""
+	Generates an extractive text summary using NLTK based on word frequency distribution.
+	
+	@param raw_text (str): The original text content to be summarized.
+	@return summary (str): The concatenated string of the top 7 ranked sentences.
+	"""
 	stopWords = set(stopwords.words("english"))
 	word_frequencies = {}  
+
+	# 1. Calculate Word Frequencies
+	# Tokenize the text and count occurrences of non-stop words
 	for word in nltk.word_tokenize(raw_text):  
 	    if word not in stopWords:
 	        if word not in word_frequencies.keys():
@@ -25,24 +34,29 @@ def nltk_summarizer(raw_text):
 	        else:
 	            word_frequencies[word] += 1
 
+	# 2. Normalize Frequencies
+	# Scale word frequencies by dividing by the maximum frequency to get weighted scores
 	maximum_frequncy = max(word_frequencies.values())
 
 	for word in word_frequencies.keys():  
 	    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
 
+	# 3. Calculate Sentence Scores
+	# Score sentences by summing the weighted frequencies of their constituent words
 	sentence_list = nltk.sent_tokenize(raw_text)
 	sentence_scores = {}  
 	for sent in sentence_list:  
 	    for word in nltk.word_tokenize(sent.lower()):
 	        if word in word_frequencies.keys():
+	            # Restrict to sentences with fewer than 30 words to avoid excessive length
 	            if len(sent.split(' ')) < 30:
 	                if sent not in sentence_scores.keys():
 	                    sentence_scores[sent] = word_frequencies[word]
 	                else:
 	                    sentence_scores[sent] += word_frequencies[word]
 
-
-
+	# 4. Generate Summary
+	# Select the top 7 sentences with the highest cumulative scores
 	summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
 
 	summary = ' '.join(summary_sentences)  
